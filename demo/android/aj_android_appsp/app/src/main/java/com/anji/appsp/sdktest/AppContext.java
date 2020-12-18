633416f758ba430f285441da042b7cc82e4b3980
chenhailong <chenhailong@anji-plus.com>
2020-12-18 14:53:04 +0800

    [demo/android] host规范

--- demo/android/aj_android_appsp/app/src/main/java/com/anji/appsp/sdktest/AppContext.java
+++ demo/android/aj_android_appsp/app/src/main/java/com/anji/appsp/sdktest/AppContext.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.text.TextUtils;
 
 import com.anji.appsp.sdk.AppSpConfig;
+import com.anji.appsp.sdk.BuildConfig;
 
 public class AppContext extends Application {
     private static AppContext mInstance;
@@ -21,7 +22,7 @@ public class AppContext extends Application {
         AppSpConfig.getInstance()
                 .init(this, appKey)
                 //可修改基础请求地址
-                .setHost("http://open-appsp.anji-plus.com/sp/")
+                .setHost("http://open-appsp.anji-plus.com")
                 //正式环境可以禁止日志输出，通过Tag APP-SP过滤看日志
                 .setDebuggable(BuildConfig.DEBUG ? true : false)
                 //务必要初始化，否则后面请求会报错
